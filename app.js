c8fe7c5658dfef485a3089b3055822bef6c4ea91
Dallas Sosebee <dallas Sosebee>
2021-10-05 21:18:35 -0500

    Add Array Definations. Add random answers to each of the 4 categories.

--- app.js
+++ app.js
@@ -20,3 +20,24 @@
 //Restaurant  let restaurants = ['Italian' , 'Tex Mex' , 'Chinese' , 'Korean' , 'Sushi' , 'Steakhouse' , 'Burgers' , 'Fast Food' , 'Seafood' , 'Sandwich Shop'];
 //Transportation  let transportations = ['Car' , 'Walking' , 'Biking' , 'Taxi' , 'Horseback' , 'Motorcycle' , 'Jogging' , 'Ferry' , 'Boat' , Hitchhiking'];
 //Entertainment let entertainments = ['Movie' , 'Shopping' , 'Playing Games' , 'Reading Books' , 'Playing Golf' , 'Bowling' , 'Target Practice' , 'Jet Skiing' , 'Wine Tasting' , 'People Watching'];
+
+
+let destinations = ['The Mall' , 'The Park' , 'Downtown' , 'Golf Course' , 'Bowling Alley' , 'The Lake' , 'Football Stadium' , 'Art Gallery' , 'Museum', 'Shooting Range'];
+
+let restaurants = ['Italian' , 'Tex Mex' , 'Chinese' , 'Korean' , 'Sushi' , 'Steakhouse' , 'Burgers' , 'Fast Food' , 'Seafood' , 'Sandwich Shop'];
+
+let transportations = ['Car' , 'Walking' , 'Biking' , 'Taxi' , 'Horseback' , 'Motorcycle' , 'Jogging' , 'Ferry' , 'Boat' , 'Hitchhiking'];
+
+let entertainments = ['Movie' , 'Shopping' , 'Playing Games' , 'Reading Books' , 'Playing Golf' , 'Bowling' , 'Target Practice' , 'Jet Skiing' , 'Wine Tasting' , 'People Watching'];
+
+let randDest = destinations[Math.floor(Math.random() * destinations.length)];
+console.log (randDest);
+
+let randRest = restaurants[Math.floor(Math.random() * restaurants.length)];
+console.log (randRest);
+
+let randTran = transportations[Math.floor(Math.random() * transportations.length)];
+console.log (randTran);
+
+let randEnt = entertainments[Math.floor(Math.random() * entertainments.length)];
+console.log (randEnt);
