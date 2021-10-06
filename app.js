36ede3cea4c9f8deb501f7c09958ec4a7fd53190
Dallas Sosebee <dallas Sosebee>
2021-10-06 17:45:03 -0500

    Cleaned up answers. Add yes/no prompts.

--- app.js
+++ app.js
@@ -24,11 +24,11 @@
 
 let destinations = ['the Mall' , 'the Park' , 'the pool' , 'the Golf Course' , 'the Bowling Alley' , 'the Lake' , 'the Football Stadium' , 'the Art Gallery' , 'the Museum', 'the Shooting Range'];
 
-let restaurants = ['Italian' , 'Tex Mex' , 'Chinese' , 'Korean' , 'Sushi' , 'Steakhouse' , 'Burgers' , 'Fast Food' , 'Seafood' , 'Sandwich Shop'];
+let restaurants = ['Italian' , 'Tex Mex' , 'Chinese' , 'Korean' , 'Sushi' , 'a Steak' , 'Burgers' , 'Fast Food' , 'Seafood' , 'at theSandwich Shop'];
 
 let transportations = ['Car' , 'Walking' , 'Biking' , 'Taxi' , 'Horseback' , 'Motorcycle' , 'Jogging' , 'Ferry' , 'Boat' , 'Hitchhiking'];
 
-let entertainments = ['Movie' , 'Shopping' , 'Playing Games' , 'Reading Books' , 'Playing Golf' , 'Bowling' , 'Target Practice' , 'Jet Skiing' , 'Wine Tasting' , 'People Watching'];
+let entertainments = ['Movie' , 'Shopping' , 'Playing Games' , 'Reading Books' , 'Playing Golf' , 'Bowling' , 'Target Practicing' , 'Jet Skiing' , 'Wine Tasting' , 'People Watching'];
 
 let randDest = destinations[Math.floor(Math.random() * destinations.length)];
 console.log (randDest);
@@ -43,7 +43,15 @@ let randEnt = entertainments[Math.floor(Math.random() * entertainments.length)];
 console.log (randEnt);
 
 
-prompt("Would you like to take a trip?");
-    if ("yes"){
+prompt("Press Any Key To Begin Your Trip Plan");
+    if (" "){
         console.log("We will go to " +  randDest + ", then we will eat " + randRest + ". We will get there by way of " + randTran + ", and finally, we will end the evening, " + randEnt + ".");
     }
+
+    prompt("Would You Like To Use This Plan?");
+        if ("yes"){
+            console.log("Enjoy your day trip!");
+        }
+        else ("no")
+            prompt("I'm sorry to hear that. I will try again");
+                
