9c06bba8327138fc4df2137d0a41f19afca8ff27
Dallas Sosebee <dallas Sosebee>
2021-10-05 20:27:59 -0500

    Add assignment requirements. Create 10 answers each to the 4 possible categories

---
+++ app.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// Day Trip Generator
+//3 commits with descriptive messages
+
+
+//Arrays Needed
+
+//Destination Randomly chosen
+//Restaurant Randomly Chosen
+//Transportation Randomly Chosen
+//Entertainment Randomly Chosen
+
+//Allow user to randomly reselect any of the above
+//Allow user to confirm trip as complete if happy with selection
+//Display completed trip in console
+//All functions must be single responsibility
+
+//Destination  let destinations = ['The Mall' , 'The Park' , 'Downtown' , 'Golf Course' , 'Bowling Alley' , 'The Lake' , 'Football Stadium' , 'Art Gallery' , 'Museum', 'Shooting Range'];
+//Restaurant  let restaurants = ['Italian' , 'Tex Mex' , 'Chinese' , 'Korean' , 'Sushi' , 'Steakhouse' , 'Burgers' , 'Fast Food' , 'Seafood' , 'Sandwich Shop'];
+//Transportation  let transportations = ['Car' , 'Walking' , 'Biking' , 'Taxi' , 'Horseback' , 'Motorcycle' , 'Jogging' , 'Ferry' , 'Boat' , Hitchhiking'];
+//Entertainment let entertainments = ['Movie' , 'Shopping' , 'Playing Games' , 'Reading Books' , 'Playing Golf' , 'Bowling' , 'Target Practice' , 'Jet Skiing' , 'Wine Tasting' , 'People Watching'];
